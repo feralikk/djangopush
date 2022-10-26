41d30a2677e04cf0a37a6e5f4cbaf9c4273e718a
Dmitry <66134845+feralikk@users.noreply.github.com>
2022-10-26 09:38:47 +0300

    debian settings for DB

--- djangopush/settings.py
+++ djangopush/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_push',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
+        'USER': 'django_push',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '',
         }
     }
 
