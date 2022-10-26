8fe104a75dd962ec462d1417ae26ac0b5fb42eed
qwerty <feralikk@mail.ru>
2022-10-26 08:35:45 +0300

    settings for nginx Debian

--- djangopush/settings.py
+++ djangopush/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 # Default primary key field type
